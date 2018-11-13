e855d681d01b91816d210a008559b9be691aa11c
Bob Bao <bob313@vt.edu>
2018-11-13 15:00:55 -0500

    all

---
+++ src/CommandProcessor.java
@@ -0,0 +1,198 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author cdc97 bob313
+ * @version 9.5.18
+ *          Processes command file and feeds instructions Hash and
+ *          MemoryManager.
+ * 
+ */
+
+public class CommandProcessor {
+
+    /**
+     * 
+     * Constructor for CommandProcessor. Takes inputs from main method and
+     * processes the commands.
+     * 
+     * @param hashSize
+     *            size of Hash received from main method
+     * 
+     * @param file
+     *            the input file containing the commands to be read and
+     *            processed
+     * 
+     */
+    public CommandProcessor(String hashSize, String file) {
+        Scanner scan = null;
+        try {
+            scan = new Scanner(new File(file));
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Command File Not Found");
+            e.printStackTrace();
+        }
+        while (scan.hasNextLine()) {
+            processCommand(scan.nextLine());
+        }
+    }
+
+
+    /**
+     * 
+     * Processes a given line of the command file for the command found in the
+     * line
+     * 
+     * @param commandString
+     *            the line of the command file being processed
+     * 
+     * @return true if proper command present false if not
+     * 
+     */
+    private boolean processCommand(String commandString) {
+        // Analyze the input string and figure out which command
+        String[] inputs = commandString.trim().split("\\s+");
+        if (inputs[0].equals("add")) {
+            if (add(commandString)) {
+                System.out.println(inputs[1] + " has been added to the database");
+            }
+            return true;
+        }
+        else if (inputs[0].equals("delete")) {
+                delete(commandString);
+            return true;
+        }
+        else if (inputs[0].equals("print")) {
+            print(commandString);
+            return true;
+        }
+        else if (inputs[0].equals("rangeprint")) {
+            rangeprint(commandString.replaceFirst("rangeprint", ""));
+        }
+        else if (inputs[0].equals("collisions")) {
+            collisions(commandString.replaceFirst("collisions", ""));
+        }
+        else if (inputs[0].equals("intersect")) {
+            intersect(commandString.replaceFirst("intersect", ""));
+        }
+        return false;
+    }
+
+    /**
+     * Handles the add command
+     * 
+     * @param addCommand
+     *            add command string
+     * @return
+     *         true if proper rating
+     */
+    private boolean add(String addCommand) {
+        String name = addCommand.replaceFirst("add", "");
+        String[] inputs = name.trim().split("\\s+");
+        return true;
+    }
+
+
+    /**
+     * Handles the delete command
+     * 
+     * @param deleteCommand
+     *            delete command string
+     */
+    private void delete(String deleteCommand) {
+        String name = deleteCommand.replaceFirst("delete", "");
+        String[] inputs = name.trim().split("\\s+");
+        if (false) {
+            System.out.println("Deleted |" + inputs[0] + "| from the database");
+        }
+        else {
+            System.out.println("Object |" + inputs[0] + "| not in the database");
+        }
+    }
+
+    /**
+     * handles the print commands
+     * 
+     * @param printCommand
+     *            print command string
+     */
+    private void print(String printCommand) {
+        String[] inputs = printCommand.trim().split("\\s+");
+        if (inputs[1].equals("skiplist")) {
+            System.out.println("SkipList dump:");
+        }
+        else if (inputs[1].equals("bintree")) {
+            System.out.println("Bintree dump:");
+        }
+        else if (inputs[1].equals("object")) {
+            if (false) {
+                System.out.println("|" + inputs[2] + "| does not exist in the database");
+            }
+            else {
+                System.out.println("Found: Drone " + inputs[2]);
+            }
+        }
+    }
+    
+    /**
+     * handles the rangeprint commands
+     * 
+     * @param rangeprintCommand
+     *            rangeprint command string
+     */
+    private void rangeprint(String rangeprintCommand) {
+        String[] inputs = rangeprintCommand.trim().split("\\s+");
+        System.out.println("Range print");
+    }
+    
+    /**
+     * handles the collision commands
+     * 
+     * @param collisionCommand
+     *            collision command string
+     */
+    private void collisions(String collisionCommand) {
+        String[] inputs = collisionCommand.trim().split("\\s+");
+        System.out.println("The following collisions exist in the database:");
+    }
+    
+    /**
+     * handles the intersect commands
+     * 
+     * @param intersectCommand
+     *            intersect command string
+     */
+    private void intersect(String intersectCommand) {
+        String[] inputs = intersectCommand.trim().split("\\s+");
+        System.out.println("The following objects intersect (" + inputs[1] + " " + inputs[2] + " " + inputs[3] + " " + inputs[4] + " " + inputs[5] + " " + inputs[6] + "):");
+    }
+
+
+    /**
+     * Formats input strings to remove excess spaces and command words (i.e.
+     * update, add, delete)
+     * 
+     * @param nameString
+     *            unformatted string
+     * @return formatted string
+     */
+    private String formatString(String nameString) {
+        StringBuilder newString = new StringBuilder();
+        char[] chars = nameString.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (!Character.isWhitespace(chars[i])) {
+                while (i < chars.length && !Character.isWhitespace(chars[i])) {
+                    newString.append(chars[i]);
+                    i++;
+                }
+                newString.append(" ");
+            }
+        }
+        newString.deleteCharAt(newString.length() - 1);
+        return newString.toString();
+    }
+
+}
