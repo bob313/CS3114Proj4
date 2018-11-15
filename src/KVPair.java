ab8929501b679df6abc3652038e379d8907f05f3
cdcarminucci <33530613+cdcarminucci@users.noreply.github.com>
2018-11-14 19:38:03 -0500

    Create KVPair.java

---
+++ src/KVPair.java
@@ -0,0 +1,29 @@
+
+public class KVPair {
+    private Comparable key;
+    private Object value;
+    
+    
+    public KVPair(Comparable key2, Object elem) {
+        key = key2;
+        
+    }
+
+
+    public Object getValue() { 
+        // TODO Auto-generated method stub
+        return value;
+    }
+
+
+    public Comparable getKey() {
+        // TODO Auto-generated method stub
+        return key;
+    }
+
+
+    
+    
+    
+
+}
