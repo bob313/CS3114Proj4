a8f7e8336065a34e021b4703aba54422ee21e34d
cdcarminucci <33530613+cdcarminucci@users.noreply.github.com>
2018-11-19 15:21:30 -0500

    Update SkipNode.java

--- src/SkipNode.java
+++ src/SkipNode.java
@@ -1,20 +1,19 @@
-
-public class SkipNode {
-
-    private KVPair rec;
-    private SkipNode[] forward;
+ class SkipNode<T extends Comparable<T>> {
+    private KVPair<T> rec;
+    private SkipNode<T>[] forward;
 
     public Object element() {
-      return rec.getValue();
+      return rec.value();
     }
 
-    public Comparable key() {
-      return rec.getKey();
+    public Comparable<T> key() {
+      return rec.key();
     }
 
-    public SkipNode(Comparable key, Object elem, int level) {
-      rec = new KVPair(key, elem);
-      setForward(new SkipNode[level + 1]);
+    @SuppressWarnings("unchecked")
+    public SkipNode(Comparable<T> key, Object elem, int level) {
+      rec = new KVPair<T>(key, elem);
+      forward = new SkipNode[level + 1];
       for (int i = 0; i < level; i++)
         getForward()[i] = null;
     }
@@ -23,12 +22,9 @@ public class SkipNode {
       return rec.toString();
     }
 
-    public SkipNode[] getForward() {
+    public SkipNode<T>[] getForward() {
         return forward;
     }
 
-    public void setForward(SkipNode[] forward) {
-        this.forward = forward;
-    }
-
-}
+    
+  }
