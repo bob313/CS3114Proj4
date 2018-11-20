2df86f0ed756fd2c715c8f00371687e4e846f64f
cdcarminucci <33530613+cdcarminucci@users.noreply.github.com>
2018-11-19 23:04:07 -0500

    Create KVPairTest.java

---
+++ src/KVPairTest.java
@@ -0,0 +1,11 @@
+public class KVPairTest extends student.TestCase {
+    
+    /**
+     * Tests the value and key getter methods
+     */
+    public void testGetters() {
+        KVPair<String> pair = new KVPair<String>("Christian", 42);
+        assertEquals("Christian", pair.key());
+        assertEquals(42, pair.value());
+    }
+}
