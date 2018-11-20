6e3cff01d6a909a5b682395f8126924168e568ea
cdcarminucci <33530613+cdcarminucci@users.noreply.github.com>
2018-11-19 23:04:36 -0500

    Create SkipNodeTest.java

---
+++ src/SkipNodeTest.java
@@ -0,0 +1,22 @@
+
+public class SkipNodeTest extends student.TestCase{
+    
+    /**
+     * Tests the element, key, and getForward methods
+     */
+    public void testGetters() {
+        SkipNode<String> node = new SkipNode<String>("Christian", 42, 0);
+        assertEquals("Christian", node.key());
+        assertEquals(42, node.element());
+        assertEquals(1, node.getForward().length);
+        assertNull(node.getForward()[0]);
+    }
+
+    /**
+     * Tests the toSting method of SkipNode
+     */
+    public void testToString() {
+        SkipNode<String> node = new SkipNode<String>("Christian", 42, 0);
+        assertEquals("Christian 42", node.toString());
+    }
+}
